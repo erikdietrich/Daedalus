e694526a5072dfe35346a1040e5be15b9405d245
Erik Dietrich <erik@daedtech.com>
2013-11-13 01:12:27 -0600

    Implemented rest service calling CM19A driver

--- AutomationService/rest.py
+++ AutomationService/rest.py
@@ -1,5 +1,9 @@
 import web
 
+from CM19aDriver import CM19aDevice
+
+REFRESH = 1.0               # Refresh rate (seconds) for polling the transceiver for inbound commands
+
 urls = (
   '/', 'index',
   '',  'index',
@@ -13,7 +17,19 @@ class index:
 		
 class office:
 	def GET(self, name):
-		print name
+		cm19a = CM19aDevice()       # Initialise device. Note: auto receiving in a thread is turned off for this example
+		if cm19a.initialised:
+			result = cm19a.send('A', '1', name)
+			result = cm19a.send('A', '2', name)
+			result = cm19a.send('A', '3', name)
+			result = cm19a.send('A', '4', name)
+			result = cm19a.send('A', '5', name)
+			result = cm19a.send('A', '6', name)
+			result = cm19a.send('A', '7', name)
+			result = cm19a.send('A', '8', name)
+		else:
+			print "Error initialising the CM19a...exiting..."
 		
+		cm19a.finish()
 if __name__ == "__main__":
     app.run()
