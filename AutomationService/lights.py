38b42ddac360fc8692b9384add8f93514894daa1
Erik Dietrich <erik@daedtech.com>
2014-03-28 06:26:06 +0000

    Finished rework of design to use lights only, removed spurious files.

--- AutomationService/lights.py
+++ AutomationService/lights.py
@@ -1,15 +1,24 @@
 import web
 import json
 
+from CM19aDriver import CM19aDevice
+
 class Lights:
 
-	def GET(self):
+	def __init__(self):
+		try:
+			open('house.json', 'r')
+
+		except IOError:
+			self.createDefaultJson()
+
+	def GET(self): #Return the list of lights
 		jsonData = self.getFileJson()
 		lights = jsonData['lights']
 		web.header('Content-Type', 'application/json')
 		return json.dumps(jsonData)
 
-	def PUT(self): #Add the room and return 200 or some server error if room exists
+	def POST(self): #Add a new light
 
 		global app
 
@@ -20,13 +29,27 @@ class Lights:
 
 		self.writeFileJson(jsonData)
 
-	def DELETE(self, lightId): #Delete a room from the database
+	def DELETE(self, lightId): #Delete a light
 		jsonData = self.getFileJson()
 		
 		updatedLights = [light for light in jsonData['lights'] if light['lightId'] != lightId]
 		jsonData['lights'] = updatedLights
 
 		self.writeFileJson(jsonData)
+
+	def PUT(self, lightId, command):
+		cm19a = CM19aDevice()
+		jsonData = self.getFileJson()
+		light = next(node for node in jsonData['lights'] if node['lightId'] == lightId)
+
+		result = cm19a.send(light['houseCode'], light['unitCode'], command)
+		cm19a.finish()
+
+	def createDefaultJson(self):
+		jsonFile = open('house.json', 'w+')
+		defaultJson = json.dumps({"lights":[]})
+		jsonFile.write(defaultJson)
+
 
 	def writeFileJson(self, jsonData):
 		with open("house.json", "w") as jsonFile:
--- AutomationService/rest.py
+++ AutomationService/rest.py
@@ -10,48 +10,14 @@ REFRESH = 1.0               # Refresh rate (seconds) for polling the transceiver
 
 urls = (
   '/lights', 'Lights',
+  '/lights/(.*?)/(.*?)', 'Lights',
   '/lights/(.*?)', 'Lights',
-  '/office/(on|off)', 'office',
-  '/room', 'room_redirect',
-  '/room/(.*?)', 'room',
   '/', 'Index',
   '/(.*?)', 'Index',
   '',  'Index')
 
 app = web.application(urls, globals())
   
-
-class room_redirect: #This is temporary
-	def GET(self):
-		raise web.seeother('/room/')
-		
-class room: # This is temporary
-    def GET(self, name):
-        print "User asked for rooms list."
-
-    def PUT(self, name):
-        print "User tried to put " + name
-
-class office: #This is temporary and will be moved to a light controller
-	def GET(self, name):
-		cm19a = CM19aDevice()       # Initialise device. Note: auto receiving in a thread is turned off for this example
-		if cm19a.initialised:
-			result = cm19a.send('A', '1', name)
-		else:
-			print "Error initialising the CM19a...exiting..."
-		
-		cm19a.finish()
-
-class dynamic:
-	def GET(self): # Show all lights in this room
-		print "User wants all lights in room " 
-
-	def PUT(self, nameOfLight): # This is going to be how users add lights to a room
-		print "Request to add light named " + nameOfLight + " to room " +	 web.ctx.path
-
-	def POST(self, nameOfLight, state): #This is going to be how users turn lights on and off 
-		print "User wants to set light " + nameOfLight + " in room " + web.ctx.path + " to state " + state
-
 
 if __name__ == "__main__":
     app.run()
